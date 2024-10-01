7606a8900aebf0d71245b4743453494aa9e9eec5
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-10-01 11:54:03 -0700

    remove template macro

--- Sources/ImageCache/ImageCache.swift
+++ Sources/ImageCache/ImageCache.swift
@@ -1,14 +1,2 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "ImageCacheMacros", type: "StringifyMacro")
-
 @attached(peer, names: arbitrary)
 public macro ImageCache() = #externalMacro(module: "ImageCacheMacros", type: "ImageCacheMacro")
--- Sources/ImageCacheMacros/ImageCacheMacro.swift
+++ Sources/ImageCacheMacros/ImageCacheMacro.swift
@@ -6,28 +6,6 @@ import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import AppKit
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
-    public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.arguments.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-
-        return "(\(argument), \(literal: argument.description))"
-    }
-}
-
 enum ImageCacheError: CustomStringConvertible, Error {
 	case internalError
 	
@@ -94,7 +72,6 @@ public struct ImageCacheMacro: PeerMacro {
 @main
 struct ImageCachePlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
 		ImageCacheMacro.self
     ]
 }
